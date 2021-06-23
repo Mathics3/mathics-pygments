58a33fedc2dc009359aa22b759f0bfa24cd4dddc
rocky <rb@dustyfeet.com>
2021-06-23 11:50:45 -0400

    Black shouldn't format version.py

--- mathics_pygments/version.py
+++ mathics_pygments/version.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-
 # This file is suitable for sourcing inside POSIX shell as
 # well as importing into Python. That's why there is no
 # space around "=" below.
+
+# fmt: off
 __version__="1.0.3.dev0"  # noqa
